d4701978acdadadbb81a288dcb0d6dcf2d375ed0
madol-abraham <m.madol@alustudent.com>
2023-11-04 08:17:06 +0000

    0-gather_data_from_an_API.py

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+import requests
 Employee EMPLOYEE_NAME is done with tasks(NUMBER_OF_DONE_TASKS/TOTAL_NUMBER_OF_TASKS):
-EMPLOYEE_NAME: Ali
-NUMBER_OF_DONE_TASKS:6
+EMPLOYEE_NAME: Ali johnson
+NUMBER_OF_DONE_TASKS: 6/1o
 TOTAL_NUMBER_OF_TASKS:10
 
-completed_tasks = ["Task 1", "Task 2", "Task 3", "Task 4", "Task 5", "Task 6"]  # list of completed tasks
+completed_tasks = ["Task 1", "Task 2", "Task 3","Task 4", "Task 5", "Task 6" ]  # tasks
 
 for task in completed_tasks:
     formatted_task = "\t " + task  # Add 1 tabulation and 1 space before the task title
     print(formatted_task)
+
+print(f"Employee {employee_name} is done with tasks ({done_tasks}/{total_tasks}):")
+
 
